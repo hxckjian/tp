dd2b27066c22b6093888fe0e709531021a2809e5
Teh Hock Jian <hockjianteh@gmail.com>
2026-03-21 21:01:47 +0800

    Create new prefixes for final product attributes

--- src/main/java/seedu/blockbook/logic/parser/CliSyntax.java
+++ src/main/java/seedu/blockbook/logic/parser/CliSyntax.java
@@ -6,12 +6,23 @@ package seedu.blockbook.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("name/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    // public static final Prefix PREFIX_NAME = new Prefix("name/");
+    // public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    // public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    // public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    // public static final Prefix PREFIX_TAG = new Prefix("t/");
+    // public static final Prefix PREFIX_GAMERTAG = new Prefix("gamertag/");
+
     public static final Prefix PREFIX_GAMERTAG = new Prefix("gamertag/");
+    public static final Prefix PREFIX_NAME = new Prefix("name/");
+    public static final Prefix PREFIX_PHONE = new Prefix("phone/");
+    public static final Prefix PREFIX_EMAIL = new Prefix("email/");
+    public static final Prefix PREFIX_GROUP = new Prefix("group/");
+    public static final Prefix PREFIX_SERVER = new Prefix("server/");
+    public static final Prefix PREFIX_FAVOURITE = new Prefix("favourite/");
+    public static final Prefix PREFIX_COUNTRY = new Prefix("country/");
+    public static final Prefix PREFIX_REGION = new Prefix("region/");
+    public static final Prefix PREFIX_NOTE = new Prefix("note/");
 
 }
 
