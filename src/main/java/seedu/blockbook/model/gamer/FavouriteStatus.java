2092bebb40b5dfc31baff00211d44dc6f1cc0d70
Teh Hock Jian <hockjianteh@gmail.com>
2026-03-21 21:03:53 +0800

    Create FavouriteStatus.java for enum values

---
+++ src/main/java/seedu/blockbook/model/gamer/FavouriteStatus.java
@@ -0,0 +1,10 @@
+package seedu.blockbook.model.gamer;
+
+/**
+ * Supported favourite status values for a gamer.
+ * FAV represents a favourite gamer, while UNFAV represents a non-favourite gamer.
+ */
+public enum FavouriteStatus {
+    FAV,
+    UNFAV
+}
